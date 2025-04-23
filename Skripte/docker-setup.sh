532661988eb1f4d9debf2e69a9e9d2dedd4ecd75
Florian in der Beek <florian.inderbeek@tecbits.de>
2025-04-23 11:32:48 +0200

    added ddev setup

--- Skripte/docker-setup.sh
+++ Skripte/docker-setup.sh
@@ -23,5 +23,23 @@ sudo apt update
 # Installation von Docker
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# Setup DDEV
+# Add DDEV’s GPG key to your keyring
+sudo sh -c 'echo ""'
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://pkg.ddev.com/apt/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/ddev.gpg > /dev/null
+sudo chmod a+r /etc/apt/keyrings/ddev.gpg
+
+# Add DDEV releases to your package repository
+sudo sh -c 'echo ""'
+echo "deb [signed-by=/etc/apt/keyrings/ddev.gpg] https://pkg.ddev.com/apt/ * *" | sudo tee /etc/apt/sources.list.d/ddev.list >/dev/null
+
+# Update package information and install DDEV
+sudo sh -c 'echo ""'
+sudo apt-get update && sudo apt-get install -y ddev
+
+# One-time initialization of mkcert
+mkcert -install
+
 # Hinzufügen des aktuellen Benutzers zur Docker-Gruppe (erfordert Neuanmeldung)
 sudo usermod -aG docker student
